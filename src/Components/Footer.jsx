78fcb2ee9bb79784a15d0e6d9da268acd53bb283
SSMudly <sm.23ee8040@nitdgp.ac.in>
2025-02-10 23:46:24 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -25,12 +25,12 @@ function Footer(){
                   strokeWidth="2"
                   className="w-5 h-5"
                   viewBox="0 0 24 24"
-                  href="https://www.instagram.com/shayantan_biswas101204/"
+                  href="https://www.facebook.com/radionitroz.nitdgp/"
                 >
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
               </a>
-              <a className="ml-3 text-white hover:text-blue-400" href="https://www.instagram.com/nitdgp1960/">
+              <a className="ml-3 text-white hover:text-blue-400" href="https://www.instagram.com/radionitroz.nitdgp/">
                 <svg
                   fill="currentColor"
                   strokeLinecap="round"
@@ -65,7 +65,7 @@ function Footer(){
                   strokeWidth="0"
                   className="w-5 h-5"
                   viewBox="0 0 24 24"
-                  href="https://x.com/saeindia_nitdgp?mx=2"
+                  href="https://www.linkedin.com/company/radio-nitroz-nitdgp/posts/?feedView=all"
                 >
                   <path
                     stroke="none"
